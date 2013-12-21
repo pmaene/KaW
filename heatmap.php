0bdf82675b614f8eaf440341c3d46c66ea954477
Stijn Meul <stijn.meul@gmail.com>
2013-12-21 20:03:32 +0100

    Added all parameters on visual maps for determining the popularity score

--- heatmap.php
+++ heatmap.php
@@ -4,6 +4,10 @@ const SALE = "sale";
 const FLAT = "flat";
 const HOUSE = "house";
 const BUSINESS = "business";
+const RESTAURANT = "restaurants";
+const HOTEL = "hotels";
+const SHOP = "shops";
+const CAFE = "cafes";
 
 $data = unserialize(file_get_contents('/tmp/city'));
 
@@ -16,11 +20,39 @@ function getNormalisedPrices($typeOfBuilding, $rentOrSale) {
             $totalNormPrice = $totalNormPrice + $houseForSale['normalisedPrice'];
 
         $nbOfHousesForSale = count($square['realEstate'][$typeOfBuilding][$rentOrSale]);
+        $house['lat'] = $square['coordinates']['minLatitude'];
+        $house['lon'] = $square['coordinates']['minLongitude'];
         if(0 != $nbOfHousesForSale) {
-            $house['lat'] = $square['coordinates']['minLatitude'];
-            $house['lon'] = $square['coordinates']['minLongitude'];
             $house['count'] = $totalNormPrice/$nbOfHousesForSale;
-            $data_js[] = $house;
+            $data_js[] = $house;   
+        }
+    }
+
+    return json_encode($data_js);
+}
+
+function getNbOfPhotos() {
+    global $data;
+    $data_js = [];
+    foreach ($data['squares'] as $square) {
+        $photos['lat'] = $square['coordinates']['minLatitude'];
+        $photos['lon'] = $square['coordinates']['minLongitude'];
+        $photos['count'] = count($square['photos']);
+        $data_js[] = $photos;
+    }
+
+    return json_encode($data_js);
+}
+
+function getNbOfOSMData($osmType) {
+    global $data;
+    $data_js = [];
+    foreach ($data['squares'] as $square) {
+        $nbOfOSMData = $osm['count'] = count($square[$osmType]);
+        if($nbOfOSMData != 0) {
+            $osm['lat'] = $square['coordinates']['minLatitude'];
+            $osm['lon'] = $square['coordinates']['minLongitude'];
+            $data_js[] = $osm;
         }
     }
 
@@ -38,7 +70,8 @@ function getMaxCountOf($jsonString) {
 
 function normalise($array, $maxValue) {
     foreach ($array as $value)
-        $value->count = $value->count/$maxValue;
+        if($value->count!=0)
+            $value->count = $value->count/$maxValue;
     return $array;
 }
 ?>
@@ -143,13 +176,42 @@ function normalise($array, $maxValue) {
                 <p>These prices are all sale prices from businesses, houses and<br /> flats divided by their maximum value and plotted on one map.</p>
                 <div id="normalisedSale" class="map"></div>
             </div>
+            <div class="mapContainer rightMap">
+                <h4>Normalised Rent Prices</h4>
+                <p>These prices are all rent prices from businesses, houses and<br /> flats divided by their maximum value and plotted on one map.</p>
+                <div id="normalisedRent" class="map"></div>
+            </div>
+            <div class="mapContainer">
+                <h4>Number of Photos</h4>
+                <div id="nbOfPhotos" class="map"></div>
+            </div>
+            <div class="mapContainer rightMap">
+                <h4>Number of Restaurants</h4>
+                <div id="nbOfRestaurants" class="map"></div>
+            </div>
+            <div class="mapContainer">
+                <h4>Number of Hotels</h4>
+                <div id="nbOfHotels" class="map"></div>
+            </div>
+            <div class="mapContainer rightMap">
+                <h4>Number of Cafes</h4>
+                <div id="nbOfCafes" class="map"></div>
+            </div>
+            <div class="mapContainer">
+                <h4>Number of Shops</h4>
+                <div id="nbOfShops" class="map"></div>
+            </div>
+            <div class="mapContainer rightMap">
+                <h4>Normalised OSM Data</h4>
+                <div id="normalisedOSM" class="map"></div>
+            </div>
         </div>
 
         <script src="http://openlayers.org/api/OpenLayers.js"></script>
         <script type="text/javascript" src="heatmap.js"></script>
         <script type="text/javascript" src="heatmap-openlayers.js"></script>
         <script type="text/javascript">
-            function createMap(testData, divName) {
+            function createMap(testData, divName, radiusVar) {
                 var transformedTestData = { max: testData.max , data: [] },
 
                 data = testData.data,
@@ -174,7 +236,7 @@ function normalise($array, $maxValue) {
                     layer,
                     {
                         visible: true,
-                        radius: 13
+                        radius: radiusVar
                     },
                     {
                         isBaseLayer: false,
@@ -203,7 +265,7 @@ function normalise($array, $maxValue) {
                     data: <?php echo $normHouseSalePrices; ?>
                 };
                 var divName = "normalisedHouseSale";
-                createMap(testData, divName);
+                createMap(testData, divName, 13);
 
                 <?php
                     $normHouseRentPrices = getNormalisedPrices(HOUSE,RENT);
@@ -215,7 +277,7 @@ function normalise($array, $maxValue) {
                     data: <?php echo $normHouseRentPrices; ?>
                 };
                 divName = "normalisedHouseRent";
-                createMap(testData, divName);
+                createMap(testData, divName, 13);
 
                 <?php
                     $normFlatSalePrices = getNormalisedPrices(FLAT,SALE);
@@ -227,7 +289,7 @@ function normalise($array, $maxValue) {
                     data: <?php echo $normFlatSalePrices; ?>
                 };
                 divName = "normalisedFlatSale";
-                createMap(testData, divName);
+                createMap(testData, divName, 13);
 
                 <?php
                     $normFlatRentPrices = getNormalisedPrices(FLAT,RENT);
@@ -239,7 +301,7 @@ function normalise($array, $maxValue) {
                     data: <?php echo $normFlatRentPrices; ?>
                 };
                 divName = "normalisedFlatRent";
-                createMap(testData, divName);
+                createMap(testData, divName, 13);
 
                 <?php
                     $normBusinessSalePrices = getNormalisedPrices(BUSINESS,SALE);
@@ -251,7 +313,7 @@ function normalise($array, $maxValue) {
                     data: <?php echo $normBusinessSalePrices; ?>
                 };
                 divName = "normalisedBusinessSale";
-                createMap(testData, divName);
+                createMap(testData, divName, 13);
 
                 <?php
                     $normBusinessRentPrices = getNormalisedPrices(BUSINESS,RENT);
@@ -263,7 +325,7 @@ function normalise($array, $maxValue) {
                     data: <?php echo $normBusinessRentPrices; ?>
                 };
                 divName = "normalisedBusinessRent";
-                createMap(testData, divName);
+                createMap(testData, divName, 13);
 
                 <?php
                     $normHouseSalePrices = normalise(json_decode($normHouseSalePrices), $maxHouseSalePrice);
@@ -277,7 +339,96 @@ function normalise($array, $maxValue) {
                     data: <?php echo json_encode($normSalePrices); ?>
                 };
                 divName = "normalisedSale";
-                createMap(testData, divName);
+                createMap(testData, divName, 13);
+
+                <?php
+                    $normHouseRentPrices = normalise(json_decode($normHouseRentPrices), $maxHouseRentPrice);
+                    $normFlatRentPrices = normalise(json_decode($normFlatRentPrices), $maxFlatRentPrice);
+                    $normBusinessRentPrices = normalise(json_decode($normBusinessRentPrices), $maxBusinessRentPrice);
+                    $normRentPrices = array_merge($normHouseRentPrices, $normFlatRentPrices, $normBusinessRentPrices);
+                ?>
+
+                testData = {
+                    max: 1,
+                    data: <?php echo json_encode($normRentPrices); ?>
+                };
+                divName = "normalisedRent";
+                createMap(testData, divName, 13);
+
+                <?php
+                    $nbOfPhotos = getNbOfPhotos();
+                    $maxNbOfPhotos = getMaxCountOf($nbOfPhotos);
+                ?>
+
+                testData = {
+                    max: <?php echo $maxNbOfPhotos; ?>,
+                    data: <?php echo $nbOfPhotos; ?>
+                };
+                divName = "nbOfPhotos";
+                createMap(testData, divName, 11);
+
+                <?php
+                    $nbOfRestaurants = getNbOfOSMData(RESTAURANT);
+                    $maxNbOfRestaurants = getMaxCountOf($nbOfRestaurants);
+                ?>
+
+                testData = {
+                    max: <?php echo $maxNbOfRestaurants; ?>,
+                    data: <?php echo $nbOfRestaurants; ?>
+                };
+                divName = "nbOfRestaurants";
+                createMap(testData, divName, 13);
+
+                <?php
+                    $nbOfHotels = getNbOfOSMData(HOTEL);
+                    $maxNbOfHotels = getMaxCountOf($nbOfHotels);
+                ?>
+
+                testData = {
+                    max: <?php echo $maxNbOfHotels; ?>,
+                    data: <?php echo $nbOfHotels; ?>
+                };
+                divName = "nbOfHotels";
+                createMap(testData, divName, 13);
+
+                <?php
+                    $nbOfCafes = getNbOfOSMData(CAFE);
+                    $maxNbOfCafes = getMaxCountOf($nbOfCafes);
+                ?>
+
+                testData = {
+                    max: <?php echo $maxNbOfCafes; ?>,
+                    data: <?php echo $nbOfCafes; ?>
+                };
+                divName = "nbOfCafes";
+                createMap(testData, divName, 13);
+
+                <?php
+                    $nbOfShops = getNbOfOSMData(SHOP);
+                    $maxNbOfShops = getMaxCountOf($nbOfShops);
+                ?>
+
+                testData = {
+                    max: <?php echo $maxNbOfShops; ?>,
+                    data: <?php echo $nbOfShops; ?>
+                };
+                divName = "nbOfShops";
+                createMap(testData, divName, 13);
+
+                <?php
+                    $nbOfShops = normalise(json_decode($nbOfShops), $maxNbOfShops);
+                    $nbOfCafes = normalise(json_decode($nbOfCafes), $maxNbOfCafes);
+                    $nbOfRestaurants = normalise(json_decode($nbOfRestaurants), $maxNbOfRestaurants);
+                    $nbOfHotels = normalise(json_decode($nbOfHotels), $maxNbOfHotels);
+                    $normOSMData = array_merge($nbOfShops, $nbOfCafes, $nbOfRestaurants, $nbOfHotels);
+                ?>
+
+                testData = {
+                    max: 1,
+                    data: <?php echo json_encode($normOSMData); ?>
+                };
+                divName = "normalisedOSM";
+                createMap(testData, divName, 11);
             }
 
             window.onload = function(){
